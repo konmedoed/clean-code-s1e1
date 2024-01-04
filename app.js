f1798173b1dfc417020c5f73b2c7ce06f7dc9faa
Kirill Kostin <konmedoed@gmail.com>
2024-01-04 20:42:49 +0300

    fix(extended-1.2): add attriibute alt for img tags

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt = "delete task";
     deleteButtonImg.className = "button_delete-icon";
     deleteButton.appendChild(deleteButtonImg);
 
